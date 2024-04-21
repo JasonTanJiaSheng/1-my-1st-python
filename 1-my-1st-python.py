ba6c1666ac260d5eff803408fdc83640810a6046
Jason Tan Jia Sheng <jasontanjiasheng@Jasons-Air.realtek>
2024-04-21 23:11:24 +0800

    first commit

---
+++ 1-my-1st-python.py
@@ -0,0 +1,58 @@
+#name = input("What is your name? :")
+#age = int(input("How old are you? :" ))
+#height = float(input("How tall are you? :"))
+
+#print("Hello " + name)
+#print("You are " + str (age) + " years old")
+#print("You are " + str (height) + " cm tall")
+
+# import math
+
+#pi = -3.14
+#x = 1
+#y = 2
+#z = 3
+
+#print(round(pi))
+#print(math.ceil(pi))
+#print(math.floor(pi))
+#print(abs(pi))
+#print(pow(pi,2))
+#print(math.sqrt(420))
+#print(max(x,y,z))
+#print(min(x,y,z))
+
+# slicing = create a substring by extracting elements from another string 
+# indexing[] or slice()
+# [start:stop:step]
+
+#name = "Jason Tan"
+
+#first_name = name[:5]          # [0:5]
+#last_name = name[6:]           # [6:end]
+#funky_name = name[::1]         # [0:end:1]
+#reversed_name = name[::-1]     # [0:end:-1]
+
+#print(reversed_name)
+
+#website1 = "http://google.com"
+#website2 = "http://wikipedia.com"
+
+#slice = slice(7,-4)
+#print(website1[slice])
+#print(website2[slice])
+
+# if statement = a block of code that will execute if it's condition is true
+
+#age = int(input("How old are you?: "))
+
+#if age == 100:
+    #print("You are a century old!") 
+#elif age >= 18:
+    #print("You are an adult!")
+#elif age < 0:
+    #print("You haven't been born yet!")    
+#else:
+    #print("You are a child!")
+
+# logical operators, (and,or, not) = used to check if two or more conditional statements
